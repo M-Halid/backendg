f75ecff7864cd4ef913bc18bdf5a1abfc9b138d9
M Halid Kutsal <mhalidkutsal@gmail.com>
2020-07-23 21:30:31 +0200

    newone

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const monk = require("monk")
 const app = express()
 
 
-const db = monk('mongodb+srv://Halid:Mongo4534.@cluster0.7e6me.mongodb.net/score?retryWrites=true&w=majority')
+const db = monk('mongodb+srv://Halid:4534Mongo.@cluster0.7e6me.mongodb.net/score?retryWrites=true&w=majority')
+db.then(() => {
+    console.log("connected")
+}
+).catch((err) => {
+    console.log("database connection failed")
+    console.log(err)
+})
+
 const score = db.get("score")
 
 
@@ -13,7 +21,9 @@ app.enable('trust proxy');
 app.use(cors())
 app.use(express.json())
 
+
 app.get("/score", (req, res) => {
+
     res.json({
         message: "Miyaw hi haloo asd heyyy🐈"
     })
@@ -21,20 +31,26 @@ app.get("/score", (req, res) => {
 
 
 
-app.get("/", (req, res, next) => {
+app.get("/open", (req, res, next) => {
+    console.log("HI!")
+
     score
-        .find()
+        .find({})
         .then(score => {
+            console.log("HI wieder!")
             res.json(score)
             console.table(score)
         }).catch((err) => {
             console.log("Fehler:" + err)
             next()
         });
+
+
+
 })
 
 
-const createScore = ("/", (req, res, next) => {
+const createScore = (req, res, next) => {
 
 
     const data = {
@@ -52,12 +68,20 @@ const createScore = ("/", (req, res, next) => {
 
 
 }
-)
+
 
 app.post('/', createScore)
 
+app.use((error, req, res, next) => {
+    res.status(500);
+    res.json({
+        message: error.message
+    });
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
 
+const server = app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
 
+server.timeout = 1000
